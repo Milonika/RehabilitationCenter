e90b9550a3ace8c933bf79c2e6cc5f71c488c353
алина алина <fkbyf-2003zv@mail.ru>
2022-04-18 11:56:48 +0300

    Create full Main Auth Window

--- Rehabilitation Center/AuthMainPage.xaml.cs
+++ Rehabilitation Center/AuthMainPage.xaml.cs
@@ -23,5 +23,32 @@ namespace Rehabilitation_Center.AuthPages
         {
             InitializeComponent();
         }
+
+        private void BtnGetStarted_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void oNas_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void ServisLabel_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void SpesialistLabel_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ContactLabel_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
